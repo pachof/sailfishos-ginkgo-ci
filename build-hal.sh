ff36ac6d731cbc62ffa31bd94be464f709109131
pachof <85188243+pachof@users.noreply.github.com>
2023-11-21 13:53:35 -0500

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -10,4 +10,5 @@ breakfast $DEVICE
 echo "clean .repo folder"
 rm -rf $ANDROID_ROOT/.repo
 
-make -j$(nproc --all) hybris-hal $(external/droidmedia/detect_build_targets.sh $PORT_ARCH $(gettargetarch)) libbiometry_fp_api_32 fake_crypt hybris/mw/sailfish-fpd-community/rpm/copy-hal.sh
+make -j$(nproc --all) hybris-hal droidmedia
+# make -j$(nproc --all) hybris-hal $(external/droidmedia/detect_build_targets.sh $PORT_ARCH $(gettargetarch)) libbiometry_fp_api_32 fake_crypt hybris/mw/sailfish-fpd-community/rpm/copy-hal.sh
